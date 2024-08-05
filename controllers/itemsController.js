fbaeafcf3f521c32ae20d00cbc755dde8e795e8c
Luca <lucatonello097@gmail.com>
2024-08-05 11:12:32 -0300

    implement adding items inside a category

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -20,7 +20,7 @@ async function getItems(req, res) {
             FROM items  
             WHERE category = $1
         `, [category]);
-        res.render("items", { title: 'items', items: items.rows});
+        res.render("items", { title: 'items', items: items.rows, category: category});
     } catch(err) {
         console.error('Error fetching items:', err);
         res.status(500).send('Server error');
@@ -40,7 +40,33 @@ async function addCategoryPost(req, res) {
 
         res.redirect('/');
     } catch (err) {
-        console.log('Error adding new category to the database', err);
+        console.error('Error adding new category to the database', err);
+        res.status(500).send('Server error');
+    }
+}
+async function addItemGet(req, res) {
+    const category = req.params.category;
+    res.render("add-item", { title: 'Add item', category: category});
+}
+async function addItemPost(req, res) {
+    try {
+        const itemName = req.body.itemName;
+        const category = req.body.category;
+        console.log('Item Name:', itemName); 
+        console.log('Category:', category);
+
+        if (!itemName || !category) {
+            throw new Error('category or name missing');
+        }
+        
+        await db.query(`
+            INSERT INTO
+            items (name, category)
+            VALUES ($1, $2)
+        `, [itemName, category]);
+        res.redirect('/' + category + '/items');
+    } catch (err) {
+        console.error('Error adding item to the category', err)
         res.status(500).send('Server error');
     }
 }
@@ -49,5 +75,7 @@ module.exports = {
     getCategories,
     getItems,
     addCategoryGet,
-    addCategoryPost
+    addCategoryPost,
+    addItemGet,
+    addItemPost
 } 
--- routes/itemsRoute.js
+++ routes/itemsRoute.js
@@ -3,8 +3,13 @@ const router = express.Router();
 const itemsController  = require('../controllers/itemsController');
 
 router.get('/', itemsController.getCategories);
+
 router.get('/:category/items', itemsController.getItems);
+
 router.get('/add-category', itemsController.addCategoryGet);
 router.post('/', itemsController.addCategoryPost);
+
+router.get('/:category/add-item', itemsController.addItemGet);
+router.post('/add-item', itemsController.addItemPost);
 
 module.exports = router;
